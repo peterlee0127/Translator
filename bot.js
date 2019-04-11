d2228b2a161b79184e129b66057ac372d39a73e3
PeterLee <peterlee0127@gmail.com>
2019-04-11 22:02:48 +0800

    try login and try to get receipt

--- bot.js
+++ bot.js
@@ -1,10 +1,55 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
-  await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
-  await page.pdf({path: 'hn.pdf', format: 'A4'});
+const options = {
+  'headless': false,  // no gui browser
+  'userDataDir':"./data" // save browser data,session,cookie
+};
 
-  await browser.close();
+
+(async () => {
+  try{
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+  
+
+
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36');
+    await page.setViewport({ width: 1280, height: 800 })
+
+
+  let dashboardPage = 'https://gengo.com/c/dashboard/'
+  await page.goto(dashboardPage);
+
+  await page.waitFor(500);
+
+  let logined = true;
+
+  if(!logined){
+    console.log('try login');
+    await page.waitForSelector('.form-group');
+
+    await page.type('.form-control', account)
+    await page.type('.real-password', password)
+    await page.click('.btn-primary')
+  }
+  await page.waitForSelector('.large-details-link');
+
+  await page.click('.large-details-link')
+  
+  await page.waitForSelector('.receipt');
+  await page.click('.receipt')
+
+  await page.waitForSelector('.quote-address-to');
+
+  await page.type('.quote-address-to', account)
+
+  await page.waitForSelector('.btn-group');
+  await page.click('.btn-group')
+
+  // await browser.close();
+  
+}catch(e) {
+    console.error(e);
+  }
+
 })();
