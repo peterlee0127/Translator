de649b76179f2017626e26c9f3945eaf76486d39
PeterLee <peterlee0127@gmail.com>
2019-04-12 21:58:18 +0800

    update for get one receipt

--- bot.js
+++ bot.js
@@ -19,8 +19,10 @@ const logined = true;
   const password = authInfo.password;
 
   await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36');
-    await page.setViewport({ width: 1280, height: 800 })
+  await page.setViewport({ width: 1280, height: 800 })
 
+  browser.addListener('targetchanged',function(event){
+  });
 
   let firstPage = 'https://gengo.com/c/dashboard/'
   if(!logined) {
@@ -41,29 +43,51 @@ const logined = true;
     await page.click('.btn-primary')
   }
   await page.waitForSelector('.large-details-link');
-  
-
-  let pageList = await browser.targets();    
-  // console.log("NUMBER TABS:", pageList);
-  //_targetInfo.title
-  //_targetInfo.url
 
   await page.click('.large-details-link')
  
   
   await page.waitForSelector('.receipt');
-  await page.click('.receipt')
+  await page.click('.receipt',{clickCount:4})
+  
+  await page.waitFor(500);
 
-  pageList = await browser.targets();    
-  pageList.forEach(item=> {
-    console.log(`${item._targetInfo.title} ${item._targetInfo.url}`)
-  });
+  pageList = await browser.pages();   
+  for(let i=0;i<pageList.length;i++) {
+    let subPage = pageList[i];
+    let SubTitle = await subPage.title();
+    let SubUrl = await subPage.url(); 
+    if(SubUrl.includes('receipt_job')){
+      await subPage.waitFor('.area-customer');
+      await subPage.evaluate(function() {
+        document.querySelector('textarea').value = ''
+      })
 
-  await page.waitFor('.area-customer');
+      await subPage.type('textarea', account, {delay: 2})
 
-  await page.type('textarea', account, {delay: 200})
+      await subPage.waitFor(1000);
+      await subPage.click('#download-button')
+      await subPage.waitFor(5000);
+      await subPage.close();
+      //download-button
+    }
+
+    // console.log(`page:${item._targetInfo.title} ${item._targetInfo.url}`)
+    // let filename = item._targetInfo.title.split(':')[0];
+
+    // bro = item.browser();
+    // console.log(item.url())
+    // let subPage = await item.page()
+
+  }
+  
+  
 
 
+    //.screenshot({path: `example-${filename}.png`});
+
+
+    
   // await page.type('form textarea', account);
 
   // await page.waitForSelector('.btn-group');
